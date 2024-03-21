e70567a88d708f66ba33276af1528ed3d6af61e3
Cha Seoyeon <seoyeon0833@gmail.com>
2024-03-21 10:14:59 +0900

    [0321] Template 패턴 공부

---
+++ Practice/ch03/Main.java
@@ -0,0 +1,21 @@
+package Practice.ch03;
+
+public class Main {
+    public static void main(String[] args) {
+        AbstractDisplay cd = new CharDisplay('a');
+
+        // cd.open();
+        // cd.print();
+        // cd.close();
+
+        cd.display(); // 템플릿 메소드를 호출
+
+        AbstractDisplay sd = new StringDisplay("덕성여대");
+
+        // sd.open();
+        // sd.print();
+        // sd.close();
+
+        sd.display();
+    }
+}
